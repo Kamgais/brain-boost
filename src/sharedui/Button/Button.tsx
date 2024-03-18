46b72ae52aee47e8c84936c805858617b6443da7
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2024-03-18 22:39:59 +0100

    init brainboost app

---
+++ src/sharedui/Button/Button.tsx
@@ -0,0 +1,11 @@
+import classes from './button.module.scss'
+
+function Button() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Button
---
+++ src/sharedui/Text/Text.tsx
@@ -0,0 +1,11 @@
+import classes from './text.module.scss'
+
+function Text() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Text
