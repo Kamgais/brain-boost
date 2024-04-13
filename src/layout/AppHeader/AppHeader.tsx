99defc3f77b0e1b31b51d34e94e3dc914b0966aa
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2024-04-13 21:12:59 +0200

    ..

--- src/layout/AppHeader/AppHeader.tsx
+++ src/layout/AppHeader/AppHeader.tsx
@@ -11,7 +11,6 @@ function AppHeader() {
   return (
     <div className={classes.appHeader_container}>
       <AppNavbar/>
-
     <div className={classes.appHeader_headline}>
        <Typography variant="headline" theme="white">Challenge your brain everyday with <span className="animate__animated animate__bounce">BrainBoost</span></Typography> 
     </div>
